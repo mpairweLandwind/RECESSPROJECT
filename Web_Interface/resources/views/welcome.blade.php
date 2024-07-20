1a72638dd2a874d196f11c6278f6994366f4bba5
mpairweLandwind <mpairwelauben22@gmail.com>
2024-07-20 11:37:16 +0300

    merged commit

--- Web_Interface/resources/views/welcome.blade.php
+++ Web_Interface/resources/views/welcome.blade.php
@@ -9,8 +9,17 @@
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />  
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
+    <!-- Styles -->
+    @livewireStyles
     <!-- Styles -->
     <style>
         /* ! tailwindcss v3.4.1 | MIT License | https://tailwindcss.com */
@@ -903,43 +912,49 @@
 </head>
 
 
-<body class="font-sans antialiased dark:bg-black dark:text-white/90">
-    <div class="bg-slate-200 text-white/30 dark:bg-slate dark:text-white/90">
+<body class="font-sans antialiased dark:bg-slate-500 dark:text-white/90">
+    <div class="bg-slate-700  text-white/30 dark:bg-slate dark:text-white/90">
         <img id="background" class="absolute -left-20 top-0 max-w-[899px]"
             src="https://laravel.com/assets/img/welcome/background.svg" />
         <div
             class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#f1281d] selection:text-white">
             <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
-                <header class="grid grid-cols-2 items-center gap-4 py-10 lg:grid-cols-3">
-                    <div class="flex justify-center lg:col-start-2">
-                        <img src="{{ Vite::asset('resources/images/logo.png') }}" class="img-fluid rounded-circle"
-                            alt="Logo" style="width: 120px; height: 120px; border-radius: 50%;">
-                    </div>
-                    @if (Route::has('login'))
-                        <nav class="col-span-2 lg:col-span-3 flex justify-end items-center mx-auto">
-                            <div class="flex space-x-4">
-                                @auth
-                                    <a href="{{ url('/dashboard') }}"
-                                        class="inline-block rounded-md font-extrabold text-xl px-4 py-2 text-white bg-[#FF2D20] hover:bg-black/70 hover:scale-105 transition-transform duration-300 focus:outline-none focus-visible:ring-[#FF2D20] dark:bg-[#FF2D20] dark:hover:bg-white/80 dark:focus-visible:ring-white">
-                                        Dashboard
-                                    </a>
-                                @else
-                                    <a href="{{ route('login') }}"
-                                        class="rounded-md font-extrabold text-2lg px-3 py-0 text-white ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/90 dark:focus-visible:ring-white">
-                                        <h1 class="text-4xl font-bold text-gray-800 dark:text-white">Log in</h1>
-
-                                    </a>
-                                    @if (Route::has('register'))
-                                        <a href="{{ route('register') }}"
-                                            class="rounded-md  font-extrabold text-xlg px-3 py-0 text-white ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/90 dark:focus-visible:ring-white">
-                                            <h1 class="text-4xl font-bold text-gray-800 dark:text-white"> Register</h1>
-                                        </a>
-                                    @endif
-                                @endauth
-                            </div>
-                        </nav>
+            <header class="grid grid-cols-2 items-center gap-4 py-10 lg:grid-cols-3">
+    <div class="flex justify-center lg:col-start-2">
+        <img 
+            src="{{ Vite::asset('resources/images/logo.png') }}" 
+            class="img-fluid rounded-circle"
+            alt="Logo" 
+            style="width: 120px; height: 120px; border-radius: 50%;">
+    </div>
+    @if (Route::has('login'))
+        <nav class="col-span-2 lg:col-span-3 flex justify-end items-center mx-auto">
+            <div class="flex space-x-4">
+                @auth
+                    <a 
+                        href="{{ url('/dashboard') }}"
+                        class="inline-block rounded-md font-extrabold text-xl px-4 py-2 text-white bg-[#FF2D20] hover:bg-black/70 hover:scale-105 transition-transform duration-300 focus:outline-none focus-visible:ring-[#FF2D20] dark:bg-[#FF2D20] dark:hover:bg-white/80 dark:focus-visible:ring-white">
+                        Dashboard
+                    </a>
+                @else
+                    <a 
+                        href="{{ route('login') }}"
+                        class="rounded-md font-extrabold text-2lg px-3 py-0 text-slate-700 ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/90 dark:focus-visible:ring-white">
+                        <h1 class="text-4xl font-bold text-gray-900 dark:text-white">Log in</h1>
+                    </a>
+                    @if (Route::has('register'))
+                        <a 
+                            href="{{ route('register') }}"
+                            class="rounded-md font-extrabold text-xlg px-3 py-0 text-slate-700 ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/90 dark:focus-visible:ring-white">
+                            <h1 class="text-4xl font-bold text-gray-900 dark:text-white">Register</h1>
+                        </a>
                     @endif
-                </header>
+                @endauth
+            </div>
+        </nav>
+    @endif
+</header>
+
 
                 <main class="mt-6 bg-slate-200">
                     <div class="grid gap-6 lg:grid-cols-2 lg:gap-8  bg-slate-400 ">
@@ -1068,6 +1083,8 @@
         </div>
 
     </div>
+
+    @livewireScripts
 </body>
 
 </html>
